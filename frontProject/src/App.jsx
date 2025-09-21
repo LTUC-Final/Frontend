f6ea14be46aa1ba611d6a2070c72725a09a7cd8f
oquraan <oquraan52@gmail.com>
2025-09-21 13:23:47 +0300

    just add some route pafe

--- frontProject/src/App.jsx
+++ frontProject/src/App.jsx
@@ -8,6 +8,21 @@ function App() {
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/" element={<h1>Alquraan </h1>} />
+
+        <Route path="/register" element={<LoginPage />} />
+        <Route path="/providerProfile" element={<h1>yaaaaa mar7aba </h1>} />
+        <Route path="/providerProfile" element={<h1>يا اهلا </h1>} />
+
+        <Route path="/providerDashboard" element={<h1>شد حالك </h1>} />
+        <Route path="/userDashboard" element={<h1>كفووو </h1>} />
+        <Route path="/mainDashBoard" element={<h1>محدا اخذها هاي </h1>} />
+        <Route path="/favorite" element={<h1>اويلييييييييييييييي </h1>} />
+        <Route path="/cart" element={<h1>Alquraan </h1>} />
+        <Route path="/payments" element={<h1>ييييييييييييييييييييي </h1>} />
+        <Route
+          path="/prodactInfo/:prodactId"
+          element={<h1>sssssssssssss</h1>}
+        ></Route>
       </Routes>
     </>
   );
