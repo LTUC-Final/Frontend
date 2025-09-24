1bfa552778d02adece8e4365229eb49b874a41a4
Omar Essam Quraan <87034745+oquraan@users.noreply.github.com>
2025-09-24 23:42:12 +0300

    Update App.jsx

--- frontProject/src/App.jsx
+++ frontProject/src/App.jsx
@@ -5,7 +5,6 @@ import Footer from "./component/NavigationBar/Footer/Footer";
 import Layout from "./component/NavigationBar/Layout";
 import NotFoundPage from "./component/notFoundPage";
 import LoginPage from "./pages/login";
-import GitAllProduct from "./component/GitAllProduct";;
 import CardDeatils from "./component/CardDetails";
 import Logout from "./pages/logout";
 import Profile from "./pages/Profile";
