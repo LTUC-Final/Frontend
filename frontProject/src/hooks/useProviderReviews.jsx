d5f2b81dcc22363644cf6c4bff7b839a535476b4
hussam-200 <hussamibrahim434@gmail.com>
2025-10-26 23:36:26 +0300

    edit review in profile provider

--- frontProject/src/hooks/useProviderReviews.jsx
+++ frontProject/src/hooks/useProviderReviews.jsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-export default function useProviderReviews(providerId, refreshTrigger) {
+export default function useProviderReviews(providerId, refreshTrigger ,isMyReview) {
   const [reviews, setReviews] = useState([]);
   const port = import.meta.env.VITE_PORT;
-
+  console.log("asdwww",reviews)
   useEffect(() => {
     if (!providerId) return;
 
@@ -20,12 +20,14 @@ export default function useProviderReviews(providerId, refreshTrigger) {
     }
 
     fetchReviews();
-  }, [providerId, refreshTrigger]);
+  }, [providerId, refreshTrigger , isMyReview]);
 
   const avgRating =
     reviews.length > 0
       ? reviews.reduce((sum, r) => sum + r.rating, 0) / reviews.length
       : null;
 
-  return { reviews, avgRating };
+  return { reviews, avgRating , setReviews  };
 }
+
+ 
