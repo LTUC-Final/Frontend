cb94693d5542b582e6760c038f2d8c5fcf53705b
hussam-200 <hussamibrahim434@gmail.com>
2025-11-08 21:57:05 +0300

    asd

--- frontProject/src/main.jsx
+++ frontProject/src/main.jsx
@@ -1,5 +1,5 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
