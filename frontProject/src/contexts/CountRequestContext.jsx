d7427186322287877e24b2e88d440bf4f0a80c0c
hussam-200 <hussamibrahim434@gmail.com>
2025-11-09 00:43:31 +0300

    asd

--- frontProject/src/contexts/CountRequestContext.jsx
+++ frontProject/src/contexts/CountRequestContext.jsx
@@ -2,6 +2,10 @@ import { createContext } from "react";
 
 const defaultValue = {
   value: 0,
+  reqItem: 0
 };
 
 export const CountRequest = createContext(defaultValue);
+
+// Provide a default Context.Provider value
+CountRequest.defaultValue = defaultValue;
